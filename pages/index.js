d533900c98f23a90516687dfe427db1777913034
chiagoziem <52287428+chiagoziem-code@users.noreply.github.com>
2023-04-28 09:43:33 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,40 +17,7 @@ const Home = () => {
             <h2>insert your subtitle here</h2>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
-          </div>
-        </a>
-      </div>
-    </div>
-  );
-};
-
-export default Home;
-
-
-
-const Home = () => {
-  return (
-    <div className="root">
-      <div className="container">
-        <div className="header">
-          <div className="header-title">
-            <h1>sup, insert your headline here</h1>
-          </div>
-          <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
-          </div>
-        </div>
-        {/* Add this code here*/}
+     {/* Add this code here*/}
         <div className="prompt-container">
           <textarea placeholder="start typing here" className="prompt-box" />
         </div>
@@ -72,5 +39,8 @@ const Home = () => {
 };
 
 export default Home;
+
+     
+
 
 
